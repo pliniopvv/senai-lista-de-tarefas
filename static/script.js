a52ecd9cc5250f2e64889097cee29b8135fd3a03
Plínio Pvv <gangss2@hotmail.com>
2024-02-17 18:40:37 -0300

    .

--- static/script.js
+++ static/script.js
@@ -1,39 +1,51 @@
 function createNote() {
   let lista = document.querySelector("#lista");
   let content = document.querySelector("#content").value;
+  document.querySelector("#content").value = "";
 
   let hr = document.createElement("hr");
+
   let li = document.createElement("li");
   let div1 = document.createElement("div");
   let div2 = document.createElement("div");
   let input = document.createElement("input");
-  let label = document.createElement("label");
   let div3 = document.createElement("div");
+  let div4 = document.createElement("div");
+  let p = document.createElement("p");
+  let div5 = document.createElement("div");
   let button = document.createElement("button");
   let i = document.createElement("i");
 
   li.appendChild(div1);
   div1.appendChild(div2);
   div2.appendChild(input);
-  div2.appendChild(label);
+
   li.appendChild(div3);
-  div3.appendChild(button);
+  div3.appendChild(div4);
+  div4.appendChild(p);
+
+  li.appendChild(div5);
+  div5.appendChild(button);
   button.appendChild(i);
 
-  label.appendChild(document.createTextNode(content));
+  p.appendChild(document.createTextNode(content));
 
   li.classList.add("ui", "grid");
-  div1.classList.add("twelve", "wide", "column");
+  div1.classList.add("two", "wide", "column");
   div2.classList.add("ui", "checkbox");
   input.type = "checkbox";
   input.tabindex = "0";
   input.classList.add("hidden");
-  //label.classList.add();
-  div3.classList.add("four", "wide", "column");
+
+  div3.classList.add("ten", "wide", "column");
+  div4.classList.add("myDescription");
+  //p.classList.add();
+
+  div5.classList.add("four", "wide", "column");
   //button.classList.add();
   i.classList.add("trash", "icon");
 
-  lista.appendChild(hr);
+  if (lista.children.length != 0) lista.appendChild(hr);
   lista.appendChild(li);
 
   button.addEventListener("click", () => {
